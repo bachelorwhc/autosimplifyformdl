06f352c1a9b620415f82988001e94acc65f666ba
bachelorwhc <bachelor.whc@gmail.com>
2018-11-15 10:46:47 +0800

    updated.

--- reduct.bat
+++ reduct.bat
@@ -1,7 +1,10 @@
 @echo off
 for /r %%f in (*.mdl) do (
 	echo %%f
-	start /b /w ./autosimplifyformdl64.exe "%%f" "%%~nf_reduct_" "%%~nf.mdl.r" > "%%~nf.r_list"
-	start /b /w ./autosimplifyformdl64.exe "%%~nf.mdl.r" "%%~nf_rreduct_" "%%~nf.mdl.rr" > "%%~nf.rr_list"
-	start /b /w ./autosimplifyformdl64.exe "%%~nf.mdl.rr" "%%~nf_rrreduct_" "%%~nf.mdl.rrr" > "%%~nf.rrr_list"
+	start /b /w ./autosimplifyformdl64.exe "%%f" "r1_" "%%~nf.mdl.r1" > "%%~nf.r1_list"
+	start /b /w ./autosimplifyformdl64.exe "%%~nf.mdl.r1" "r2_" "%%~nf.mdl.r2" > "%%~nf.r2_list"
+	start /b /w ./autosimplifyformdl64.exe "%%~nf.mdl.r2" "r3_" "%%~nf.mdl.r3" > "%%~nf.r3_list"
+	start /b /w ./autosimplifyformdl64.exe "%%~nf.mdl.r3" "r4_" "%%~nf.mdl.r4" > "%%~nf.r4_list"
+	start /b /w ./autosimplifyformdl64.exe "%%~nf.mdl.r4" "r5_" "%%~nf.mdl.r5" > "%%~nf.r5_list"
+	start /b /w ./autosimplifyformdl64.exe "%%~nf.mdl.r5" "r6_" "%%~nf.mdl.r6" > "%%~nf.r6_list"
 )
